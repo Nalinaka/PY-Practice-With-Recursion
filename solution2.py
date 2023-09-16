ab84d4def74b42e055216954cd713c07405fd595
Nalinaka <nalinaka@gmail.com>
2023-09-16 12:16:46 -0700

    Python Recursion Practice

--- solution2.py
+++ solution2.py
@@ -1,1 +1,12 @@
-# Write code for algorithm 2 below
+def find_factorial(natural_numbers):
+    if natural_numbers == 10:
+        return 10
+    
+    else:
+        return (number + find_factorial(number +1))
+        
+
+print(find_factorial(10))
+              
+    # natural_numbers(3)
+    # output: 1 2 3
--- solution3.py
+++ solution3.py
@@ -1,1 +1,15 @@
-# Write code for algorithm 3 below
+def fibonacci_sequence(n):
+
+  #  Base case
+    if n==0:
+        return
+
+  #  Recursive case
+    else:
+      print(n)
+    
+    fibonacci_sequence(n * 2)
+
+# test case
+n=88
+fibonacci_sequence(n)
